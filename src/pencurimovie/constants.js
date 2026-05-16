52ee906ede7c5d501f21faef32363ff6112c839d
osahpanehamek <barangpanas04@gmail.com>
2026-05-16 23:29:52 +0800

    Add files via upload

---
+++ src/pencurimovie/constants.js
@@ -0,0 +1,7 @@
+export const MAIN_URL = "https://ww105.pencurimoviesubmalay.guru";
+export const HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/148.0.0.0 Safari/537.36",
+    "Cookie": "starstruck_190fe508139b417479c63b112781c8e7=6b72ea42f54a6cc0067373f7906e256b; cf_clearance=24S6uyuUe_MWmFo8BUTs8oAqOJEKjZPS2bB8D.42pZo-1778944537-1.2.1.1-a7QmgdoXgLJ_K3GNg_vBQ14IpMzKYDgHPz1ikpIL05T5Q6cWP1Ia4Mt7_awC6_V9UeXJQVwJi4fpeh_swKGMToXn0INrYPuLl6qFAI.Q.qMHFahVaj3YvkfC6A_h1oOk4IYg_KoIvCsRr5KCFt0ijgjTz9yOtjQra9rgr180vR9VnjmTXAL7n.uNy1AMgTx6bqXXlj6e8ywZJeXx3LR1JkWnsQ.lYfHz4jEuLzhXR81Un7xGe8LmsusaN4LqZj_izpO6OFXbydpetIvMB2CQ2OQ665TqaFX3wE1p8uKl5JJtD.C_YM1vW4o2EWdEpL1mcxmSHX8jxGMo.9C8T.jYLQ",
+    "Referer": "https://ww105.pencurimoviesubmalay.guru/"
+};
+export const TMDB_API_KEY = "1865f43a0549ca50d341dd9ab8b29f49";
